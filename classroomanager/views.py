f828b56ad18e1ee5752c2aedd8cd4cb53d9b5654
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2020-05-14 14:11:54 -0300

    404 response

--- classroomanager/views.py
+++ classroomanager/views.py
@@ -7,3 +7,8 @@ from classroomanager.auth.login_utils import login_required
 @login_required
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return 'Sumiu.... (lost...)'
